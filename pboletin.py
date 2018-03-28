322dbfc1b30b51bc128ad73f45fc26c3be916373
Patricio Moracho <pmoracho@gmail.com>
2018-03-28 00:37:12 -0300

    work on process_lines

--- pboletin.py
+++ pboletin.py
@@ -88,45 +88,31 @@ def detect_lines(filepath):
 	linesP = cv.HoughLinesP(clean_mask_gray,rho, np.pi/180,thres,minLineLength = minLineLength,maxLineGap = maxLineGap)
 	if linesP is not None:
 
-		top = int(125*in_res/300)
-		left= int(125*in_res/300)
-		bottom = int(3260*in_res/300)
-		right = int(2300*in_res/300)
-
 		ll = [e[0] for e in np.array(linesP).tolist()]
-
-		add_box(ll, top, bottom, right, left)
-		ll.sort()
-		remove_lines(ll,top,bottom)
-		simplificar(ll,50)
-		ll = list(e for e,_ in itertools.groupby(ll))
-
+		ll = process_lines(ll, in_res)
 		pprint.pprint(ll)
+
 		for l in [e[1] for e in enumerate(ll)]:
 			cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
 			cv.line(blank_image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv.LINE_AA)
 
 
-	cv.imwrite('final.png', cdstP)
-	blank_image = cv.cvtColor(blank_image, cv.COLOR_BGR2GRAY)
-	blank_image = cv.threshold(blank_image, 60, 255, cv.THRESH_BINARY)[1]
-	blank_image = cv.bitwise_not(blank_image)
+# 	cv.imwrite('final.png', cdstP)
+# 	blank_image = cv.cvtColor(blank_image, cv.COLOR_BGR2GRAY)
+# 	blank_image = cv.threshold(blank_image, 60, 255, cv.THRESH_BINARY)[1]
+# 	blank_image = cv.bitwise_not(blank_image)
 	cv.imwrite('blank_image.png', blank_image)
+	cv.imwrite('final.png', cdstP)
 
-	# cv.imwrite('blank_image.png', blank_image)
-	# contours = cv.findContours(blank_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-	cnts = cv.findContours(blank_image,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
-	pprint.pprint(cnts)
+# 	cnts = cv.findContours(blank_image,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+# 	pprint.pprint(cnts)
 
-	for c in cnts[1]:
-		pprint.pprint(c)
+# 	for c in cnts[1]:
+# 		pprint.pprint(c)
 
-	cv.drawContours(blank_image,cnts[1][1],-1,(0,255,0),-1)
-	cv.imwrite('contornos.png', blank_image)
-
-	# # cnts = sorted(cnts, key = cv.contourArea, reverse = True)
-	# pprint.pprint(len(contours[0]))
+# 	cv.drawContours(blank_image,cnts[1][1],-1,(0,255,0),-1)
+# 	cv.imwrite('contornos.png', blank_image)
 
 	cv.namedWindow(filepath, cv.WINDOW_NORMAL)
 	cv.resizeWindow(filepath, 600,600)
@@ -134,9 +120,80 @@ def detect_lines(filepath):
 	cv.waitKey()
 	cv.destroyAllWindows()
 
+def process_lines(lista, in_res):
+
+	top = int(240*in_res/300)
+	# left= int(125*in_res/300)
+	# bottom = int(3260*in_res/300)
+	# right = int(2300*in_res/300)
+
+	############################################################################
+	# Establezco el valor de y minímo para que caiga debajo de la recta 
+	# horizontal del titulo de página
+	############################################################################
+	lista = [[l[0],
+			  l[1] if l[1] > top else top,
+		      l[2],
+		      l[3] if l[3] > top else top]
+			  for l in lista]
+
+	############################################################################
+	# Agregago el recuadro completo de la pagina
+	############################################################################
+	puntos=[(l[0],l[1]) for l in lista]
+	puntos.extend([(l[2],l[3]) for l in lista])
+
+	min_x = min([x for x,y in puntos]) - 50
+	min_y = min([y for x,y in puntos])
+	max_x = max([x for x,y in puntos]) + 50
+	max_y = max([y for x,y in puntos])
+
+	lista.append([min_x, min_y, max_x, min_y])
+	lista.append([min_x, min_y, min_x, max_y])
+	lista.append([min_x, max_y, max_x, max_y])
+	lista.append([max_x, max_y, max_x, min_y])
+
+	############################################################################
+	# Simplifico las líneas
+	############################################################################
+	level = 55
+	aprox = {0:{}, 1:{}}
+
+	for i in (0,1):
+		valor = list(sum(list(zip(*[(e[0+i], e[2+i]) for e in lista])), ()))
+		for e in valor:
+			if e not in aprox[i]:
+				aprox[i].update({e-x: e for x in range(-level, level + 1, 1)})
+
+	for i,e in enumerate(lista):
+		lista[i][0] = aprox[0][e[0]]
+		lista[i][1] = aprox[1][e[1]]
+		lista[i][2] = aprox[0][e[2]]
+		lista[i][3] = aprox[1][e[3]]
+
+	############################################################################
+	# Ordeno y me quedo con las líneas no repetidas
+	############################################################################
+	lista.sort()
+	lista = list(e for e,_ in itertools.groupby(lista))
+
+	return(lista)
+
+def set_top(lista, maxtop):
+
+	lista = [[l[0],
+			  l[1] if l[1] > maxtop else maxtop,
+		      l[2],
+		      l[3] if l[3] > maxtop else maxtop]
+			  for l in lista]
+
+
+
+	return(lista)
+
 def remove_lines(lista, maxtop, maxbottom):
 
-	lista = [l for l in lista if l[1] > maxtop and not (l[1] > maxbottom and l[3] > maxbottom)]
+	lista = [l for l in lista if l[0] > maxtop and not (l[0] > maxbottom and l[2] > maxbottom)]
 
 
 def add_box(lista, top, bottom, right, left):
