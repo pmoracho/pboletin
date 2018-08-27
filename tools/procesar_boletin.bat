715d459f694793b03da792664770d5f6dea96985
Patricio Moracho <pmoracho@gmail.com>
2018-08-27 12:06:11 -0300

    add test images

--- tools/procesar_boletin.bat
+++ tools/procesar_boletin.bat
@@ -1,5 +1,9 @@
 @echo off
 set /p boletin="Numero de boletin a procesar: "
-@pboletin.exe %boletin%.pdf %1 %2 %3 %4 %5 %6
+@pboletin.exe %boletin%.pdf
+
+@copy \\Momdb2Test\Marcas\PruebasNip\%boletin%\JPG\*.JPG \\PlusDesa02\Mecanus\PI\Images\Import\Boletin\
+@copy \\Momdb2Test\Marcas\PruebasNip\%boletin%\PCX\*.PCX \\PlusDesa02\Mecanus\PI\Images\Import\Boletin\
+
 
 pause
